44f0d5b1a74e462d80b147a85781fa0af817bc9a
divyannshu <divyanshum3232@gmail.com>
2024-08-25 14:53:18 +0530

    basic component building

--- src/profileCard.js
+++ src/profileCard.js
@@ -1,9 +1,21 @@
-function profileCard({title,handle}){
+function profileCard({title,handle,image,description}){
     // const  {title,handle}=props;
-    return <div>
-        <div>{title}</div>
-        <div>{handle}</div>
+    return (
+    <div className="card">
+        <div className="card-image">
+            <figure className="image is-1by1">
+            <img src={image} alt="pda logo"/>
+            </figure>
+        </div>
+        <div className="card-content">
+            <div className="media-content">
+            <p className="title is-4">{title}</p>
+            <p className="subtitle is-6">{handle}</p>
+        </div>
+         <div class="content">{description}</div>
     </div>
+    </div>
+    );
 }
 
 export default profileCard;
